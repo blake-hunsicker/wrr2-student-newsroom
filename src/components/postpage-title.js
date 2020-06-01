abd2c205c1bd42a5f29f9d72f77d9bb32bbaa855
Blake Hunsicker <blakeyhunsicker@gmail.com>
2020-06-01 15:15:08 -0400

    Testing Pato's story, removing unecessary hovers from article title on article page

--- src/components/postpage-title.js
+++ src/components/postpage-title.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 
 const PostPageTitle = ({link, type, title, authorLink, authors}) => (
   <div className="post-page-title">
-    <Link to={link}>
+    <Link to={link} className="category-link">
       <h4 className="post-page-type">
         {type}
       </h4>
@@ -12,7 +12,7 @@ const PostPageTitle = ({link, type, title, authorLink, authors}) => (
       {title}
     </h1>
     <h1 className="post-page-byline">
-      A story by <Link to={authorLink}>{authors}</Link>
+      A story by <Link to={authorLink} className="author-link">{authors}</Link>
     </h1>
   </div>
 )
